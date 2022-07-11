090232d60a88ac5edb44d6866e470e0cf18e764d
maheshgondkar18 <gondkarmahesh3@gmail.com>
2022-07-11 11:37:58 +0530

    updated

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,24 +5,32 @@ export default function TextForm(props) {
     const [text,setText] = useState('Enter text here');
     // setText{""    }
     const handleUpClick =()=>{
-        console.log("UpperCase was Clicked..!"+text);
+        // console.log("UpperCase was Clicked..!"+text);
         let newText = text.toUpperCase();
         setText(newText);
     }
     const handleOnChange =(event)=>{
-        console.log("On Change");
+        // console.log("On Change");
         setText(event.target.value);
     }
 
   return (
-    <div>
+    <>
+    <div className="container">
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <label htmlFor="myBox" >TextArea</label>
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppcase</button>
-        
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppcase</button>    
     </div>
+    <div className="container my-2">
+        <h1>Your text Summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
