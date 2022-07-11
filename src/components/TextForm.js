885dddd6517428444598b230b94d1c4ef74be415
maheshgondkar18 <gondkarmahesh3@gmail.com>
2022-07-11 10:55:45 +0530

    useState+event handling

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,24 +1,28 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 export default function TextForm(props) {
+    const [text,setText] = useState('Enter text here');
+    // setText{""    }
+    const handleUpClick =()=>{
+        console.log("UpperCase was Clicked..!"+text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleOnChange =(event)=>{
+        console.log("On Change");
+        setText(event.target.value);
+    }
+
   return (
     <div>
-      <form>
-        <h1>{props.heading}</h1>
-  <div className="mb-3">
-    <label for="myBox" class="form-label">TextArea</label>
-    <textarea className="form-control" id="myBox" rows="3"></textarea>
-  </div>
-  {/* <div className="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1"/>
-  </div>
-  <div className="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-    <label className="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button> */}
-</form>
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+                <label htmlFor="myBox" >TextArea</label>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppcase</button>
+        
     </div>
   )
 }
